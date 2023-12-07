7db7ebe1c2f297270151627d697944814bb03f25
Язов Михаил <100093644+MRxPosiTiVe@users.noreply.github.com>
2023-12-07 13:19:22 +0500

    Add files via upload

--- exc1-3.c
+++ exc1-3.c
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+// Структура, представляющая товар
 struct product
 {
     char name[256];
     int price;
 };
 
+// Прототип функции сортировки массива товаров по цене
 void sort(struct product *arr, const int SIZE);
+
+// Функция для определения размера файла (количество строк)
 int getFileSize(FILE *file);
 
 int main()
 {
+    // Открытие файла "exc1-3.txt" для чтения
     FILE *file = fopen("exc1-3.txt", "r");
     if (file == NULL)
     {
+        // Вывод сообщения об ошибке, если файл не найден
         printf("Error, file was not found");
         exit(EXIT_FAILURE);
     }
+
+    // Определение размера файла (количество строк)
     int fileSize = getFileSize(file);
+
+    // Выделение динамической памяти под массив товаров
     struct product *products = malloc(fileSize * sizeof(struct product));
 
+    // Заполнение массива данными из файла и их вывод
     int i = 0;
     while (fscanf(file, "%s %d", products[i].name, &products[i].price) == 2)
     {
@@ -29,17 +40,31 @@ int main()
         printf("Name: %s; Price: %dр\n", products[i].name, products[i].price);
         i++;
     }
+
+    // Закрытие файла
     fclose(file);
+
+    // Открытие файла "exc1-3Output.txt" для записи
     file = fopen("exc1-3Output.txt", "w");
+
+    // Сортировка массива товаров по цене
     sort(products, fileSize);
+
+    // Запись отсортированных товаров в файл
     for (int i = 0; i < fileSize; i++)
         fprintf(file, "Name: %s; Cost: %dp\n", products[i].name, products[i].price);
 
+    // Закрытие файла
     fclose(file);
+
+    // Освобождение выделенной памяти для массива товаров
     free(products);
+
+    // Возвращение значения 0, что указывает на успешное завершение программы
     return 0;
 }
 
+// Функция для определения размера файла (количество строк)
 int getFileSize(FILE *file)
 {
     int lineCount = 0;
@@ -55,13 +80,15 @@ int getFileSize(FILE *file)
     fseek(file, 0, SEEK_SET);
     return lineCount;
 }
+
+// Функция сортировки массива товаров по цене (пузырьковая сортировка)
 void sort(struct product *arr, const int SIZE)
 {
-    // Sort by cost
     for (int i = 0; i < SIZE - 1; i++)
     {
         for (int j = 0; j < SIZE - i - 1; j++)
         {
+            // Поменять местами товары, если их цены стоят в неправильном порядке
             if ((arr + j)->price > (arr + j + 1)->price)
             {
                 struct product temp = *(arr + j);
--- exc1-4.c
+++ exc1-4.c
@@ -2,29 +2,31 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+// Структура для представления узла стека
 struct StackNode {
-    int data;
-    struct StackNode* next;
+    int data;               // Данные узла стека (значение элемента)
+    struct StackNode* next; // Указатель на следующий узел в стеке
 };
 
+// Структура для представления стека
 struct Stack {
-    struct StackNode* top;
-    int capacity; // v
-    int size; // size <= capacity
+    struct StackNode* top; // Указатель на верхний элемент стека
+    int capacity;          // Вместимость стека (максимальное количество элементов)
+    int size;               // Текущий размер стека (количество элементов)
 };
 
-void sAddElem(int value, struct Stack* stack); // +
-int sPopElem(struct Stack* stack); // +
-int sPeekElem(struct Stack* stack); // +
-void initStack(struct Stack* stack, int capacity); // +
-struct StackNode* sCreateElem(int data); // +
-
+// Прототипы функций для работы со стеком
+void sAddElem(int value, struct Stack* stack);
+int sPopElem(struct Stack* stack);
+int sPeekElem(struct Stack* stack);
+void initStack(struct Stack* stack, int capacity);
+struct StackNode* sCreateElem(int data);
 
 int main(void) {
-    // It's a simple test for stack struct
+    // Простой тест для структуры стека
     
     struct Stack stack;
-    initStack(&stack, 3);
+    initStack(&stack, 10);
     sAddElem(10, &stack);
     sAddElem(13, &stack);
     sAddElem(18, &stack);
@@ -33,57 +35,60 @@ int main(void) {
     sPopElem(&stack);
     sAddElem(19, &stack);
     sAddElem(11, &stack);
+
     return 0;
 }
 
+// Функция для добавления элемента в стек
 void sAddElem(int value, struct Stack *stack) {
     if (stack->size + 1 > stack->capacity) {
-        perror("Stack Overflow Exception");
+        perror("Stack Overflow Exception"); // Вывод сообщения об ошибке переполнения стека
         exit(EXIT_FAILURE);
     }
-    struct StackNode *newNode = sCreateElem(value);
-    newNode->next = stack->top;
-    stack->top = newNode;
-    stack->size++;
+    struct StackNode *newNode = sCreateElem(value); // Создание нового узла стека
+    newNode->next = stack->top; // Установка указателя next нового узла на текущий верхний узел
+    stack->top = newNode; // Установка нового узла как верхний элемент стека
+    stack->size++; // Увеличение размера стека
 }
 
+// Функция для извлечения элемента из стека
 int sPopElem(struct Stack *stack) {
     if (stack->top == NULL) {
-        perror("Stack is empty");
+        perror("Stack is empty"); // Вывод сообщения об ошибке, если стек пуст
         exit(EXIT_FAILURE);
     }
-    struct StackNode* temp = stack->top;
-    int data = temp->data;
-    stack->top = temp->next;
-    free(temp);
-    stack->size--;
-    return data;
+    struct StackNode* temp = stack->top; // Временная переменная для хранения верхнего узла
+    int data = temp->data; // Сохранение значения верхнего узла
+    stack->top = temp->next; // Установка указателя верхнего узла на следующий узел
+    free(temp); // Освобождение памяти, занятой верхним узлом
+    stack->size--; // Уменьшение размера стека
+    return data; // Возвращение значения верхнего узла
 }
 
+// Функция для просмотра верхнего элемента стека
 int sPeekElem(struct Stack *stack) {
     if (stack->top == NULL) {
-        perror("Stack is empty");
+        perror("Stack is empty"); // Вывод сообщения об ошибке, если стек пуст
         exit(EXIT_FAILURE);
     }
-    return stack->top->data;
+    return stack->top->data; // Возвращение значения верхнего узла без его извлечения
 }
 
-// *(pointer).hui = pointer->hui
-
-void initStack(struct Stack *stack, int capacity)
-{
-    stack->top = NULL;
-    stack->capacity = capacity;
+// Функция для инициализации стека
+void initStack(struct Stack *stack, int capacity) {
+    stack->top = NULL; // Установка верхнего элемента стека в NULL (стек пуст)
+    stack->capacity = capacity; // Установка вместимости стека
+    stack->size = 0; // Начальный размер стека установлен в 0
 }
 
-struct StackNode *sCreateElem(int data)
-{
-    struct StackNode* newNode = malloc(sizeof(struct StackNode));
+// Функция для создания нового узла стека
+struct StackNode *sCreateElem(int data) {
+    struct StackNode* newNode = malloc(sizeof(struct StackNode)); // Выделение памяти под новый узел
     if (newNode == NULL) {
-        perror("Failed to allocate memory");
+        perror("Failed to allocate memory"); // Вывод сообщения об ошибке, если не удалось выделить память
         exit(EXIT_FAILURE);
     }
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+    newNode->data = data; // Установка значения нового узла
+    newNode->next = NULL; // Установка указателя next в NULL (новый узел становится верхним)
+    return newNode; // Возвращение нового узла
 }
